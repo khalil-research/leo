7a10e3690246a88adcd68280b3188017fcdec3e5
rahulptel <rahul.polymtl@gmail.com>
2022-09-12 00:15:16 -0400

    [ADD] Find the best model based on total val loss

--- python/learn2rank/scripts/find_best_model.py
+++ python/learn2rank/scripts/find_best_model.py
@@ -1,22 +1,45 @@
+import argparse
 import pathlib
+import pickle as pkl
+import shutil
 
-import hydra
-from omegaconf import DictConfig, OmegaConf
-
-
-def check_cfg_match(cfg, run_cfg):
-    return True
+import numpy as np
+from omegaconf import OmegaConf
 
 
-@hydra.main(version_base=None, config_path='config', config_name='find_best_model.yaml')
-def main(cfg: DictConfig):
-    output_path = pathlib.Path(__file__).parent.joinpath('outputs')
-    for f in output_path.rglob('*/config.yaml'):
-        run_cfg = OmegaConf.load(f)
-        is_match = check_cfg_match(cfg, run_cfg)
-        if is_match:
-            pass
+def main(args):
+    # Best params tracking
+    best_path = None
+    best_loss = np.infty
+    best_epoch = 0
+    best_cfg = None
+
+    # Iterate and find experiment with best validation loss
+    output_path = pathlib.Path(args.output_dir)
+    for i, f in enumerate(output_path.rglob('*/results.pkl')):
+        print(i, f)
+        result = pkl.load(open(f, 'rb'))
+        cfg = OmegaConf.load(f.parent.joinpath('.hydra/config.yaml'))
+        cfg_yaml = OmegaConf.to_yaml(cfg)
+        if result['best']['loss']['total'] < best_loss:
+            best_path = f
+            best_loss = result['best']['loss']['total']
+            best_epoch = result['best']['epoch']
+            best_cfg = cfg_yaml
+
+    # Copy the best experiment to the pretrained_dir
+    shutil.copytree(best_path.parent,
+                    f'learn2rank/resources/pretrained/{args.pretrained_dir}',
+                    dirs_exist_ok=True)
+
+    print(f'Best loss: {best_loss:.4f}')
+    print(f'Best epoch: {best_epoch}')
+    print(best_cfg)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_dir', type=str, required=True)
+    parser.add_argument('--pretrained_dir', type=str, default='nn__vri')
+    args = parser.parse_args()
+    main(args)
