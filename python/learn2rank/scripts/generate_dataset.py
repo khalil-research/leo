5a3028f751d995d4cf467e52fca4d8d6375a60f0
rahulptel <rahul.polymtl@gmail.com>
2022-10-27 16:40:39 -0400

    [REF] Generate dataset

--- python/learn2rank/scripts/generate_dataset.py
+++ python/learn2rank/scripts/generate_dataset.py
@@ -4,13 +4,12 @@ from pathlib import Path
 
 import hydra
 import numpy as np
-from hydra.utils import get_original_cwd
 from omegaconf import DictConfig
 
 from learn2rank.featurizer.factory import featurizer_factory
-from learn2rank.utils import get_variable_rank_from_weights
-from learn2rank.utils import property_weight_dict2array
-from learn2rank.utils import read_data_from_file
+from learn2rank.utils.data import read_data_from_file
+from learn2rank.utils.order import get_variable_rank_from_weights
+from learn2rank.utils.order import property_weight_dict2array
 
 
 @hydra.main(version_base='1.1', config_path='../config', config_name='generate_dataset.yaml')
@@ -25,39 +24,32 @@ def main(cfg: DictConfig):
     -------
 
     """
-    cfg = cfg.featurizer
-    res_path = Path(get_original_cwd()).joinpath('learn2rank/resources')
-    dp = res_path / 'instances' / cfg.problem.name
-    assert dp.exists(), "Invalid dataset path!"
+    res_path = Path(cfg.res_path[cfg.machine])
     dataset = {}
 
-    # For each data folder
-    for df in dp.iterdir():
-        of = res_path / 'smac_output' / cfg.problem.name
-        if not of.exists():
-            continue
+    # For each size
+    for size in cfg.featurizer.size:
+        if size not in dataset:
+            dataset[size] = {}
 
-        # For each split
-        for dfs in df.iterdir():
-            # Add split key
-            if dfs.name not in dataset:
-                dataset[dfs.name] = {}
+        smac_out = res_path / 'smac_output' / cfg.featurizer.name / size
+        for split in smac_out.iterdir():
+            if split.stem not in dataset[size]:
+                dataset[size][split.stem] = {}
 
-            for data_file in dfs.iterdir():
-                iname = data_file.stem
-                split = data_file.parent.stem
-                ptype = data_file.parent.parent.stem
+            for inst in split.iterdir():
+                dataset[size][split.stem][inst.stem] = {'x': {}, 'y': []}
+                _dataset = dataset[size][split.stem][inst.stem]
 
-                data = read_data_from_file(data_file)
-                dataset[split][iname] = {'x': {}, 'y': []}
-                _dataset = dataset[dfs.name][iname]
-
-                featurizer = featurizer_factory.create(cfg.problem.name, cfg=cfg, data=data)
+                # Prepare x
+                dat_path = res_path / 'instances' / cfg.featurizer.name / size / f'{split.stem}/{inst.stem}.dat'
+                data = read_data_from_file(dat_path)
+                featurizer = featurizer_factory.create(cfg.featurizer.name, cfg=cfg.featurizer, data=data)
                 features = featurizer.get()
                 _dataset['x'] = features
 
-                # Load incumbent config
-                smac_out_dir = of / ptype / split / iname / f'run_{cfg.seed.smac}'
+                # Prepare y
+                smac_out_dir = inst / f'run_{cfg.featurizer.seed.smac}'
                 traj = smac_out_dir.joinpath('traj.json')
                 with open(traj, 'r') as fp:
                     lines = fp.readlines()
@@ -69,7 +61,7 @@ def main(cfg: DictConfig):
                     _dataset['y'][-1]['rank'] = get_variable_rank_from_weights(
                         data, run['incumbent'], normalized=False)
 
-    with open(res_path / f'datasets/{cfg.problem.name}.pkl', 'wb') as fp:
+    with open(res_path / f'datasets/{cfg.featurizer.name}/{cfg.featurizer.name}_dataset.pkl', 'wb') as fp:
         pkl.dump(dataset, fp)
 
 
