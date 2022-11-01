9358034fd5bbb634271b53013fb15ef279c1f9a9
rahulptel <rahul.polymtl@gmail.com>
2022-11-01 16:48:12 -0400

    [REF] Clean SMAC TA

--- python/learn2rank/scripts/smac_ta.py
+++ python/learn2rank/scripts/smac_ta.py
@@ -1,71 +1,10 @@
 import argparse
 import logging
 import sys
-from operator import itemgetter
-
-import numpy as np
 
 from learn2rank.utils.bdd import run_bdd_builder
-
-
-def read_from_file(filepath):
-    data = {'value': [], 'weight': [], 'capacity': 0}
-
-    with open(filepath, 'r') as fp:
-        n_vars = int(fp.readline())
-        n_objs = int(fp.readline())
-
-        for _ in range(n_objs):
-            data['value'].append(list(map(int, fp.readline().split())))
-
-        data['weight'].extend(list(map(int, fp.readline().split())))
-
-        data['capacity'] = int(fp.readline().split()[0])
-
-    return data
-
-
-def get_variable_score(data, feature_weights):
-    weight, value = np.asarray(data['weight']), np.asarray(data['value'])
-    n_items = weight.shape[0]
-    value_mean = np.mean(value, axis=0)
-    value_max = np.max(value, axis=0)
-    value_min = np.min(value, axis=0)
-
-    scores = np.zeros(n_items)
-    for fk, fv in feature_weights.items():
-        if fk == 'weight':
-            scores += fv * weight
-        elif fk == 'avg_value':
-            scores += fv * value_mean
-        elif fk == 'max_value':
-            scores += fv * value_max
-        elif fk == 'min_value':
-            scores += fv * value_min
-        elif fk == 'avg_value_by_weight':
-            scores += fv * (value_mean / weight)
-        elif fk == 'max_value_by_weight':
-            scores += fv * (value_max / weight)
-        elif fk == 'min_value_by_weight':
-            scores += fv * (value_min / weight)
-
-    return scores
-
-
-def get_variable_order(data, feature_weights):
-    """Returns array of variable order
-    For example: [2, 1, 0]
-    Here 2 is the index of item which should be used first to create the BDD
-    """
-    n_items = len(data['weight'])
-    scores = get_variable_score(data, feature_weights)
-
-    idx_score = [(i, v) for i, v in zip(np.arange(n_items), scores)]
-    idx_score.sort(key=itemgetter(1), reverse=True)
-
-    order = [i for i, v in idx_score]
-
-    return order, idx_score
+from learn2rank.utils.data import read_data_from_file
+from learn2rank.utils.order import get_variable_order_from_weights
 
 
 def run_target_algorithm(instance, cutoff, feature_weights, log=True):
@@ -75,11 +14,10 @@ def run_target_algorithm(instance, cutoff, feature_weights, log=True):
         logger = logging.getLogger(__file__)
         logger.debug(instance)
 
-    # num_objectives = int(instance.split("/")[-1].split("_")[2])
-    data = read_from_file(instance)
-    order, _ = get_variable_order(data, feature_weights)
-
+    data = read_data_from_file(instance)
+    order, _ = get_variable_order_from_weights(data, feature_weights)
     status, runtime = run_bdd_builder(instance, order, time_limit=cutoff, get_runtime=True)
+    
     print(f"Result for SMAC: {status}, {runtime}, 0, 0, 0")
 
 
