bb968aba2a435dd56c1846a829d6c4f8430177d5
rahulptel <rahul.polymtl@gmail.com>
2022-11-05 21:03:12 -0400

    [FIX] Generate script name

--- python/learn2rank/CC/eval_baseline_ordering.py
+++ python/learn2rank/CC/eval_baseline_ordering.py
@@ -1,29 +1,38 @@
 case = 1
-size = [(3, 60), (5, 40)]
-order_type = ['rand']
+size = [(3, 100)]
+order_type = ['min_weight']
 n_items = 50
+run_train = True
+run_val = False
+problem = 'setpacking'
 
 fp = open('table.dat', 'w')
 for s in size:
     for ot in order_type:
-        for i in range(0, 1000, n_items):
-            fp.write(f'{case} python -m learn2rank.scripts.eval_static_orderings '
-                     f'problem.n_objs={s[0]} '
-                     f'problem.n_vars={s[1]} '
-                     f'order_type={ot} '
-                     f'from_pid={i} '
-                     f'to_pid={i + n_items} '
-                     f'split=train '
-                     f'case={case}\n')
-            case += 1
+        if run_train:
+            for i in range(0, 50, n_items):
+                fp.write(f'{case} python -m learn2rank.scripts.eval_baseline_ordering '
+                         f'problem={problem} '
+                         f'problem.n_objs={s[0]} '
+                         f'problem.n_vars={s[1]} '
+                         f'order_type={ot} '
+                         f'from_pid={i} '
+                         f'to_pid={i + n_items} '
+                         f'split=train '
+                         f'machine=cc '
+                         f'case={case}\n')
+                case += 1
 
-        for i in range(1000, 1100, n_items):
-            fp.write(f'{case} python -m learn2rank.scripts.eval_static_orderings '
-                     f'problem.n_objs={s[0]} '
-                     f'problem.n_vars={s[1]} '
-                     f'order_type={ot} '
-                     f'from_pid={i} '
-                     f'to_pid={i + n_items} '
-                     f'split=val '
-                     f'case={case}\n')
-            case += 1
+        if run_val:
+            for i in range(1000, 1100, n_items):
+                fp.write(f'{case} python -m learn2rank.scripts.eval_baseline_ordering '
+                         f'problem={problem} '
+                         f'problem.n_objs={s[0]} '
+                         f'problem.n_vars={s[1]} '
+                         f'order_type={ot} '
+                         f'from_pid={i} '
+                         f'to_pid={i + n_items} '
+                         f'split=val '
+                         f'machine=cc '
+                         f'case={case}\n')
+                case += 1
