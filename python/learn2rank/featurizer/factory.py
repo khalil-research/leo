a8feb62e4455a0dce5663ddaf91cf3a68f8499ed
rahulptel <rahul.polymtl@gmail.com>
2022-11-07 10:59:01 -0500

    [ADD] Binproblem featurizer

--- python/learn2rank/featurizer/factory.py
+++ python/learn2rank/featurizer/factory.py
@@ -1,5 +1,7 @@
 from learn2rank.utils import Factory
+from .binproblem import BinproblemFeaturizer
 from .knapsack import KnapsackFeaturizer
 
 featurizer_factory = Factory()
 featurizer_factory.register_member('knapsack', KnapsackFeaturizer)
+featurizer_factory.register_member('binproblem', BinproblemFeaturizer)
