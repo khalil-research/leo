e16cc34f7868f64048df1d0d329d65f1d9242185
rahulptel <rahul.polymtl@gmail.com>
2022-11-07 10:59:31 -0500

    [REF] Gen baseline ordering table.dat

--- python/learn2rank/CC/eval_baseline_ordering.py
+++ python/learn2rank/CC/eval_baseline_ordering.py
@@ -1,38 +1,109 @@
 case = 1
-size = [(3, 100)]
-order_type = ['min_weight']
-n_items = 50
-run_train = True
-run_val = False
-problem = 'setpacking'
 
-fp = open('table.dat', 'w')
-for s in size:
+
+def create_table_line(case=0, problem='knapsack', n_objs=3, n_vars=60, split='train', order_type='canonical',
+                      from_pid=0, to_pid=1):
+    return f'{case} python -m learn2rank.scripts.eval_baseline_ordering ' \
+           f'problem={problem} ' \
+           f'problem.n_objs={n_objs} ' \
+           f'problem.n_vars={n_vars} ' \
+           f'order_type={order_type} ' \
+           f'split={split} ' \
+           f'from_pid={from_pid} ' \
+           f'to_pid={to_pid} ' \
+           f'machine=cc ' \
+           f'case={case}\n'
+
+
+def create_knapsack_table(splits=None):
+    assert splits is not None
+
+    global case
+    size = [(3, 60), (3, 50), (3, 40), (3, 30), (3, 20),
+            (4, 50), (4, 40), (4, 30), (4, 20),
+            (5, 40), (5, 30), (5, 20)]
+    order_type = ['min_weight', 'canonical']
+    n_items = 50
+
+    table_str = ''
     for ot in order_type:
-        if run_train:
-            for i in range(0, 50, n_items):
-                fp.write(f'{case} python -m learn2rank.scripts.eval_baseline_ordering '
-                         f'problem={problem} '
-                         f'problem.n_objs={s[0]} '
-                         f'problem.n_vars={s[1]} '
-                         f'order_type={ot} '
-                         f'from_pid={i} '
-                         f'to_pid={i + n_items} '
-                         f'split=train '
-                         f'machine=cc '
-                         f'case={case}\n')
-                case += 1
+        for split in splits:
+            key, active, start, end = split
+            if active:
+                for s in size:
+                    for si in range(start, end, n_items):
+                        table_str += create_table_line(case=case, problem='knapsack', n_objs=s[0], n_vars=s[1],
+                                                       split=key, from_pid=si, to_pid=si + n_items, order_type=ot)
+                        case += 1
 
-        if run_val:
-            for i in range(1000, 1100, n_items):
-                fp.write(f'{case} python -m learn2rank.scripts.eval_baseline_ordering '
-                         f'problem={problem} '
-                         f'problem.n_objs={s[0]} '
-                         f'problem.n_vars={s[1]} '
-                         f'order_type={ot} '
-                         f'from_pid={i} '
-                         f'to_pid={i + n_items} '
-                         f'split=val '
-                         f'machine=cc '
-                         f'case={case}\n')
-                case += 1
+    return table_str
+
+
+def create_setpacking_table(splits=None):
+    assert splits is not None
+
+    global case
+    size = [(3, 100), (3, 125), (3, 150),
+            (4, 100), (4, 125), (4, 150),
+            (5, 100), (5, 125)]
+    order_type = ['min_weight', 'canonical']
+    n_items = 50
+
+    table_str = ''
+    for ot in order_type:
+        for split in splits:
+            key, active, start, end = split
+            if active:
+                for s in size:
+                    for si in range(start, end, n_items):
+                        table_str += create_table_line(case=case, problem='setpacking', n_objs=s[0], n_vars=s[1],
+                                                       split=key, from_pid=si, to_pid=si + n_items, order_type=ot)
+                        case += 1
+
+    return table_str
+
+
+def create_setcovering_table(splits=None):
+    assert splits is not None
+
+    global case
+    size = [(3, 100), (3, 125),
+            (4, 100), (4, 125),
+            (5, 100)]
+    order_type = ['min_weight', 'canonical']
+    n_items = 50
+
+    table_str = ''
+    for ot in order_type:
+        for split in splits:
+            key, active, start, end = split
+            if active:
+                for s in size:
+                    for si in range(start, end, n_items):
+                        table_str += create_table_line(case=case, problem='setcovering', n_objs=s[0], n_vars=s[1],
+                                                       split=key, from_pid=si, to_pid=si + n_items, order_type=ot)
+                        case += 1
+
+    return table_str
+
+
+def main():
+    gen_knapsack = True
+    gen_setpacking = False
+    gen_setcovering = False
+
+    fp = open('table.dat', 'w')
+    splits = (('train', True, 0, 1000),
+              ('val', True, 1000, 1100),
+              ('test', False, 1100, 1200))
+
+    table_str = ''
+    table_str += create_knapsack_table(splits=splits) if gen_knapsack else table_str
+    table_str += create_setpacking_table(splits=splits) if gen_setpacking else table_str
+    table_str += create_setcovering_table(splits=splits) if gen_setcovering else table_str
+
+    fp.write(table_str)
+
+
+if __name__ == '__main__':
+    main()
