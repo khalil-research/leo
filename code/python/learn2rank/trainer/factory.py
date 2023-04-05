ba9e28ac8016700d64ca07369beca3c7ccceff68
rahulptel <rahul.polymtl@gmail.com>
2023-04-05 14:05:52 -0400

    [REF] Add SVMRank factory

--- code/python/learn2rank/trainer/factory.py
+++ code/python/learn2rank/trainer/factory.py
@@ -1,8 +1,10 @@
 from learn2rank.utils import Factory
 from .trainer_pytorch import PyTorchTrainer
 from .trainer_sklearn import SklearnTrainer
+from .trainer_svmrank import SVMRankTrainer
 
 # Factory to hold trainers
 trainer_factory = Factory()
 trainer_factory.register_member('PyTorchTrainer', PyTorchTrainer)
 trainer_factory.register_member('SklearnTrainer', SklearnTrainer)
+trainer_factory.register_member('SVMRankTrainer', SVMRankTrainer)
