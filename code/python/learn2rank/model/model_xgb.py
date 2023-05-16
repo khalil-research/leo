751855e8fcd9abc5f75f06d8f056c83ebfdb8728
rahulptel <rahul.polymtl@gmail.com>
2023-05-16 16:57:33 -0400

    [FIX] Initializing parent class

--- code/python/learn2rank/model/model_xgb.py
+++ code/python/learn2rank/model/model_xgb.py
@@ -1,12 +1,14 @@
-from xgboost import XGBRanker
 from omegaconf import OmegaConf
+from xgboost import XGBRanker
 
 
 class GradientBoostingRanker(XGBRanker):
     def __init__(self, cfg=None):
-        super(GradientBoostingRanker, self).__init__(OmegaConf.to_container(cfg, resolve=True))
+        super(GradientBoostingRanker, self).__init__(
+            **OmegaConf.to_container(cfg, resolve=True)
+        )
         self.cfg = cfg
 
     @property
     def id(self):
-        return f"xgb_nes-{self.cfg.n_estimators}_md-{self.cfg.max_depth}"
+        return f"xgb_nes-{self.cfg.n_estimators}_md-{self.cfg.max_depth}_rlam-{self.cfg.reg_lambda}"
