22be4911ac650ba84299093d6c37706cc9469562
rahulptel <rahul.polymtl@gmail.com>
2023-05-17 23:28:44 -0400

    [REF] Eval pred using best model summary

--- code/python/learn2rank/scripts/eval_pred_ordering.py
+++ code/python/learn2rank/scripts/eval_pred_ordering.py
@@ -2,6 +2,7 @@ import logging
 import os
 import pickle as pkl
 from pathlib import Path
+import pandas as pd
 
 import hydra
 from omegaconf import DictConfig
@@ -18,40 +19,63 @@ print(os.getcwd())
 @hydra.main(version_base='1.1', config_path='../config', config_name='eval_pred_ordering.yaml')
 def main(cfg: DictConfig):
     resource_path = Path(cfg.res_path[cfg.machine])
-    inst_path = resource_path.joinpath(f'instances/{cfg.problem.inst_name}')
-    preds_path = resource_path.joinpath(f'predictions/{cfg.pred.name}')
-    preds = pkl.load(open(str(preds_path), 'rb'))
-    if cfg.split == 'train':
-        preds = preds['tr']
+    inst_path = Path(cfg.problem.inst_name)
+    pred_path = Path(cfg.pred_path)
+    best_model_summary_path = Path(cfg.best_model_summary_path)
+    df_best_model = pd.load_csv(best_model_summary_path)
+
+    if cfg.model_name is None:
+        model_names = df_best_model['model_name'].values
     else:
-        preds = preds['val']
-    # preds = preds['val'] if cfg.split is None else preds[cfg.split]
+        assert cfg.model_name in df_best_model['model_name'].values
+        model_names = [cfg.model_name]
+
     results = []
+    for model_name in model_names:
+        row = df_best_model[df_best_model['model_name'] == model_name]
+        prediction_path = row[0, 'prediction_path']
+        predictions = pkl.load(prediction_path)
 
-    names, n_items, order = preds['names'], preds['n_items'], preds['order']
-    for _name, _n_item, _order in zip(names, n_items, order):
-        _, _, n_objs, n_vars, pid = _name.split('_')
-        pid = int(pid)
-        if pid < cfg.from_pid or pid >= cfg.to_pid:
-            continue
+        preds = pkl.load(open(str(predictions), 'rb'))
+        if cfg.split == 'train':
+            preds = preds['tr']
+        elif cfg.split == 'val':
+            preds = preds['val']
+        else:
+            preds = preds['test']
 
-        log.info(f'Processing {_name}')
+        names, n_items, order = preds['names'], preds['n_items'], preds['order']
+        for _name, _n_item, _order in zip(names, n_items, order):
+            _, _, n_objs, n_vars, pid = _name.split('_')
+            pid = int(pid)
+            if pid < cfg.from_pid or pid >= cfg.to_pid:
+                continue
 
-        size = f'{n_objs}_{n_vars}'
-        dat_path = inst_path / size / f'{cfg.split}/{_name}.dat'
+            log.info(f'Processing {_name}')
+            dat_path = inst_path / cfg.problem.size / f'{cfg.split}/{_name}.dat'
 
-        status, result = run_bdd_builder(str(dat_path), _order[:_n_item], bin_path=str(resource_path),
-                                         prob_id=str(cfg.problem.id), preprocess=str(cfg.problem.preprocess),
-                                         time_limit=cfg.bdd.timelimit, mem_limit=cfg.bdd.memlimit)
-        results.append(make_result_column(cfg.problem.name,
-                                          size,
-                                          cfg.split,
-                                          pid,
-                                          'pred',
-                                          result))
+            status, result = run_bdd_builder(str(dat_path), _order[:_n_item], bin_path=str(resource_path),
+                                             prob_id=str(cfg.problem.id), preprocess=str(cfg.problem.preprocess),
+                                             time_limit=cfg.bdd.timelimit, mem_limit=cfg.bdd.memlimit)
+            results.append(make_result_column(cfg.problem.name,
+                                              cfg.problem.size,
+                                              cfg.split,
+                                              pid,
+                                              f'pred_{model_name}',
+                                              result))
 
-    with open(f"eval_{cfg.pred.name.split('.')[0]}_{cfg.split}_{cfg.from_pid}_{cfg.to_pid}.pkl", 'wb') as fp:
-        pkl.dump(results, fp)
+    eval_order_path = Path(cfg.eval_order)
+    eval_order_path.mkdir(parents=True, exist_ok=True)
+    eval_order_path = eval_order_path / f"pred_{cfg.split}_{cfg.from_pid}_{cfg.to_pid}.csv"
+
+    df = pd.DataFrame(results, columns=['problem', 'size', 'split', 'pid', 'order_type', 'run_id',
+                                        'nnds', 'iw', 'rw', 'inc', 'rnc', 'iac', 'rac',
+                                        'comp_time', 'red_time', 'pareto_time', 'nnds_per_layer'])
+    if eval_order_path.exists():
+        df_old = pd.read_csv(eval_order_path)
+        df = pd.concat([df, df_old], ignore_index=True)
+
+    df.to_csv(eval_order_path, index=False)
 
 
 if __name__ == '__main__':
