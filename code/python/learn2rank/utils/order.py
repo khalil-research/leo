900a8a6ca15dffb77f10d3bbab1aaf90cfc75b3b
rahulptel <rahul.polymtl@gmail.com>
2023-06-26 12:39:43 -0400

    [REF] Simplify variable rank and order utilities

--- code/python/learn2rank/utils/order.py
+++ code/python/learn2rank/utils/order.py
@@ -8,19 +8,92 @@ import numpy as np
 from .const import KnapsackPropertyWeights
 import pandas as pd
 
-def get_order(data):
-    order = {
-        'max_weight': None,
-        'min_weight': None,
-        'max_avg_value': None,
-        'min_avg_value': None,
-        'max_max_value': None,
-        'min_max_value': None,
-        'max_min_value': None,
-        'min_min_value': None,
-        'max_avg_value_by_weight': None,
-        'max_max_value_by_weight': None,
-    }
+
+def property_weight_dict2array(pw_dict, cast_to_numpy=False):
+    lst = []
+    for i in range(len(KnapsackPropertyWeights)):
+        lst.append(pw_dict[KnapsackPropertyWeights(i).name])
+
+    if cast_to_numpy:
+        lst = np.asarray(lst)
+
+    return lst
+
+
+def _score2order(scores, reverse=False):
+    """
+    Order variables based on its score
+    If reverse is False (default), then lower score will lead to a
+    higher place high in the order
+    If reverse is True, then higher score will lead to a higher place
+    in the order. Used with SVMRank.
+
+    [80, -10, 50, 100], reverse=False ==> [1, 2, 0, 3]
+    [80, -10, 50, 100], reverse=True ==> [3, 0, 2, 1]
+    """
+    orders = []
+    for _scores in scores:
+        var_rank_score = []
+        for var, score in enumerate(_scores):
+            var_rank_score.append((var, score))
+
+        var_rank_score.sort(key=itemgetter(1), reverse=reverse)
+        order = [int(var) for (var, _) in var_rank_score]
+        orders.append(order)
+
+    return orders
+
+
+def _score2rank(scores, reverse=False, high_to_low=False, normalized=False):
+    """Rank variables based on its score.
+    If reverse is False (default), then lower score will lead to a
+    higher rank
+    If reverse is True, then lower score will lead to a lower rank
+
+    [80, -10, 50, 100], reverse=False ==> [2, 0, 1, 3]
+    [80, -10, 50, 100], reverse=True ==> [1, 3, 2, 0]
+    """
+    ranks = []
+    for _scores in scores:
+        var_rank_score = []
+        for var, score in enumerate(_scores):
+            var_rank_score.append((var, score))
+
+        var_rank_score.sort(key=itemgetter(1), reverse=reverse)
+
+        n_items = len(_scores)
+        _ranks = [0] * n_items
+        for rank, (var, _) in enumerate(var_rank_score):
+            _ranks[int(var)] = rank if high_to_low is False else n_items - rank
+
+        if normalized:
+            _ranks = np.array(_ranks) / n_items
+
+        ranks.append(_ranks)
+
+    return ranks
+
+
+def get_static_orders(data, order_type=None, property_weights=None):
+    if order_type is None:
+        order = {
+            'max_weight': None,
+            'min_weight': None,
+            'max_avg_profit': None,
+            'min_avg_profit': None,
+            'max_max_profit': None,
+            'min_max_profit': None,
+            'max_min_profit': None,
+            'min_min_profit': None,
+            'max_avg_profit_by_weight': None,
+            'max_max_profit_by_weight': None,
+        }
+    elif type(order_type) == str:
+        order = {order_type: None}
+    elif type(order_type) == list:
+        order = {ot: None for ot in order_type}
+    else:
+        raise ValueError('Invalid type(order_type)...')
 
     for o in order.keys():
         if o == 'max_weight':
@@ -34,61 +107,75 @@ def get_order(data):
             idx_weight.sort(key=itemgetter(1))
             order[o] = [i[0] for i in idx_weight]
 
-        elif o == 'max_avg_value':
+        elif o == 'max_avg_profit':
             mean_profit = np.mean(data['value'], 0)
             idx_profit = [(i, mp) for i, mp in enumerate(mean_profit)]
             idx_profit.sort(key=itemgetter(1), reverse=True)
             order[o] = [i[0] for i in idx_profit]
 
-        elif o == 'min_avg_value':
+        elif o == 'min_avg_profit':
             mean_profit = np.mean(data['value'], 0)
             idx_profit = [(i, mp) for i, mp in enumerate(mean_profit)]
             idx_profit.sort(key=itemgetter(1))
             order[o] = [i[0] for i in idx_profit]
 
-        elif o == 'max_max_value':
+        elif o == 'max_max_profit':
             max_profit = np.max(data['value'], 0)
             idx_profit = [(i, mp) for i, mp in enumerate(max_profit)]
             idx_profit.sort(key=itemgetter(1), reverse=True)
             order[o] = [i[0] for i in idx_profit]
 
-        elif o == 'min_max_value':
+        elif o == 'min_max_profit':
             max_profit = np.max(data['value'], 0)
             idx_profit = [(i, mp) for i, mp in enumerate(max_profit)]
             idx_profit.sort(key=itemgetter(1))
             order[o] = [i[0] for i in idx_profit]
 
-        elif o == 'max_min_value':
+        elif o == 'max_min_profit':
             min_profit = np.min(data['value'], 0)
             idx_profit = [(i, mp) for i, mp in enumerate(min_profit)]
             idx_profit.sort(key=itemgetter(1), reverse=True)
             order[o] = [i[0] for i in idx_profit]
 
-        elif o == 'min_min_value':
+        elif o == 'min_min_profit':
             min_profit = np.min(data['value'], 0)
             idx_profit = [(i, mp) for i, mp in enumerate(min_profit)]
             idx_profit.sort(key=itemgetter(1))
             order[o] = [i[0] for i in idx_profit]
 
-        elif o == 'max_avg_value_by_weight':
+        elif o == 'max_avg_profit_by_weight':
             mean_profit = np.mean(data['value'], 0)
             profit_by_weight = [v / w for v, w in zip(mean_profit, data['weight'])]
             idx_profit_by_weight = [(i, f) for i, f in enumerate(profit_by_weight)]
             idx_profit_by_weight.sort(key=itemgetter(1), reverse=True)
+            # print(idx_profit_by_weight)
             order[o] = [i[0] for i in idx_profit_by_weight]
 
-        elif o == 'max_max_value_by_weight':
+        elif o == 'max_max_profit_by_weight':
             max_profit = np.max(data['value'], 0)
             profit_by_weight = [v / w for v, w in zip(max_profit, data['weight'])]
             idx_profit_by_weight = [(i, f) for i, f in enumerate(profit_by_weight)]
             idx_profit_by_weight.sort(key=itemgetter(1), reverse=True)
+            # print(idx_profit_by_weight)
             order[o] = [i[0] for i in idx_profit_by_weight]
+
+        elif o == 'max_min_profit_by_weight':
+            min_profit = np.min(data['value'], 0)
+            profit_by_weight = [v / w for v, w in zip(min_profit, data['weight'])]
+            idx_profit_by_weight = [(i, f) for i, f in enumerate(profit_by_weight)]
+            idx_profit_by_weight.sort(key=itemgetter(1), reverse=True)
+            # print(idx_profit_by_weight)
+            order[o] = [i[0] for i in idx_profit_by_weight]
+
+        elif o == 'smac_instance' or o == 'smac_dataset':
+            assert property_weights is not None
+            order[o] = get_variable_order(data=data, property_weights=property_weights, reverse=True)[0]
 
     return order
 
 
 def get_weighted_order(opts, data, weighted_ordering_dict):
-    orders = get_order(data)
+    orders = get_static_orders(data)
     num_items = opts.n
 
     # for o in ordering_lst:
@@ -157,187 +244,72 @@ def get_variable_score_from_weights(data, property_weights):
     return scores
 
 
-def get_variable_order_from_weights(data, property_weights):
-    """Returns array of variable order
-    For example: [2, 1, 0]
-    Here 2 is the index of item which should be used first to create the BDD
+def get_variable_order(data=None, property_weights=None, scores=None, reverse=False):
     """
-    n_items = len(data['weight'])
-    scores = get_variable_score_from_weights(data, property_weights)
+    Get the variable order by providing either `data` and `property_weights` or `scores`.
+    If `data` and `property_weights` are provided, we first calculate the scores of that instance and put it in a list.
+    Once we have `scores`, obtained either as input or calculated from `data` and `property_weights`, we calculate the
+    order based on `reverse`.
 
-    idx_score = [(i, v) for i, v in zip(np.arange(n_items), scores)]
-    idx_score.sort(key=itemgetter(1), reverse=True)
+    If `reverse` is False/True then the items are sorted in ascending/descending order.
 
-    order = [i for i, v in idx_score]
+    Parameters
+    ----------
+    data : dict or None
+        A dictionary containing the problem instance
+    property_weights : dict or None
+        A dictionary containing the weights of different properties
+    scores : list[list] or None
+        A list of list containing scores of items of different instances
+    reverse : bool, default is False
+        Flag to select sorting mechanism; False -> Ascending; True -> Descending
 
-    return order, idx_score
-
-
-def get_variable_rank_from_weights(data, property_weights, normalized=True):
-    """Returns array of variable ranks
-    For example: [2, 1, 0]
-    Item 0 must be used third to construct the BDD
+    Returns
+    -------
+    A list of list containing the order of items of different instances.
+    If the function was invoked using `data` and `property_weights`, the parent list will contain only one child list.
     """
-    n_items = len(data['weight'])
-
-    _, idx_score_desc = get_variable_order_from_weights(data, property_weights)
-
-    variable_rank = np.zeros(n_items)
-    for rank, (i, _) in enumerate(idx_score_desc):
-        variable_rank[i] = rank
-    if normalized:
-        variable_rank /= n_items
-
-    return variable_rank
-
-
-def property_weight_dict2array(pw_dict, cast_to_numpy=False):
-    lst = []
-    for i in range(len(KnapsackPropertyWeights)):
-        lst.append(pw_dict[KnapsackPropertyWeights(i).name])
-
-    if cast_to_numpy:
-        lst = np.asarray(lst)
-
-    return lst
-
-
-def pred_score2order(scores, reverse=False):
-    """
-    Order variables based on its score
-    If reverse is False (default), then lower score will lead to a
-    higher place high in the order
-    If reverse is True, then higher score will lead to a higher place
-    in the order. Used with SVMRank.
-
-    [80, -10, 50, 100], reverse=False ==> [1, 2, 0, 3]
-    [80, -10, 50, 100], reverse=True ==> [3, 0, 2, 1]
-    """
-    orders = []
-    for _scores in scores:
-        var_rank_score = []
-        for var, score in enumerate(_scores):
-            var_rank_score.append((var, score))
-
-        var_rank_score.sort(key=itemgetter(1), reverse=reverse)
-        order = [int(var) for (var, _) in var_rank_score]
-        orders.append(order)
+    scores = [get_variable_score_from_weights(data, property_weights)] if scores is None else scores
+    orders = _score2order(scores, reverse=reverse)
 
     return orders
 
 
-def pred_score2rank(scores, reverse=False):
-    """Rank variables based on its score.
-    If reverse is False (default), then lower score will lead to a
-    higher rank
-    If reverse is True, then lower score will lead to a lower rank
-
-    [80, -10, 50, 100], reverse=False ==> [2, 0, 1, 3]
-    [80, -10, 50, 100], reverse=True ==> [1, 3, 2, 0]
+def get_variable_rank(data=None, property_weights=None, scores=None, reverse=False, high_to_low=False, normalized=True):
     """
-    ranks = []
-    for _scores in scores:
-        var_rank_score = []
-        for var, score in enumerate(_scores):
-            var_rank_score.append((var, score))
+    Get the variable rank by providing either `data` and `property_weights` or `scores`.
+    If `data` and `property_weights` are provided, we first calculate the scores of that instance and put it in a list.
+    Once we have `scores`, obtained either as input or calculated from `data` and `property_weights`, we calculate ts
+    rank based on `reverse`, `high_to_low` and `normalized` parameters.
 
-        var_rank_score.sort(key=itemgetter(1), reverse=reverse)
+    If `reverse` is False/True then the items are sorted in ascending/descending order.
+    If `high_to_low` is False/True then the first element sorted based on score get a rank 0/N.
+    If `normalizes` is False/True then the rank is not divided/get divided by the number of items.
 
-        _ranks = [0] * len(_scores)
-        for rank, (var, _) in enumerate(var_rank_score):
-            _ranks[int(var)] = rank
+    Parameters
+    ----------
+    data : dict or None
+        A dictionary containing the problem instance
+    property_weights : dict or None
+        A dictionary containing the weights of different properties
+    scores : list[list] or None
+        A list of list containing scores of items of different instances
+    reverse : bool, default is False
+        Flag to select sorting mechanism; False -> Ascending; True -> Descending
+    high_to_low : bool, default is False
+        Flag to select ranking mechanism; False -> First element gets rank 0; True -> First element get rank N
+    normalized : bool, default is False
+        Flag to select normalization; False -> Do not normalize; True -> Normalize
 
-        ranks.append(_ranks)
+    Returns
+    -------
+    A list of list containing the rank of items of different instances.
+    If the function was invoked using `data` and `property_weights`, the parent list will contain only one child list.
+    """
+    scores = [get_variable_score_from_weights(data, property_weights)] if scores is None else scores
+    ranks = _score2rank(scores, reverse=reverse, high_to_low=high_to_low, normalized=normalized)
 
     return ranks
-
-
-def get_static_orders(data, order_type=None):
-    if order_type == None:
-        order = {
-            'max_weight': None,
-            'min_weight': None,
-            'max_avg_profit': None,
-            'min_avg_profit': None,
-            'max_max_profit': None,
-            'min_max_profit': None,
-            'max_min_profit': None,
-            'min_min_profit': None,
-            'max_avg_profit_by_weight': None,
-            'max_max_profit_by_weight': None,
-        }
-    else:
-        if type(order_type) == str:
-            order = {order_type: None}
-        elif type(order_type) == list:
-            order = {ot: None for ot in order_type}
-
-    n_items = len(data['weight'])
-    for o in order.keys():
-        if o == 'max_weight':
-            idx_weight = [(i, w) for i, w in enumerate(data['weight'])]
-            # print(o, idx_weight)
-            idx_weight.sort(key=itemgetter(1), reverse=True)
-            order[o] = [i[0] for i in idx_weight]
-
-        elif o == 'min_weight':
-            idx_weight = [(i, w) for i, w in enumerate(data['weight'])]
-            idx_weight.sort(key=itemgetter(1))
-            order[o] = [i[0] for i in idx_weight]
-
-        elif o == 'max_avg_profit':
-            mean_profit = np.mean(data['value'], 0)
-            idx_profit = [(i, mp) for i, mp in enumerate(mean_profit)]
-            idx_profit.sort(key=itemgetter(1), reverse=True)
-            order[o] = [i[0] for i in idx_profit]
-
-        elif o == 'min_avg_profit':
-            mean_profit = np.mean(data['value'], 0)
-            idx_profit = [(i, mp) for i, mp in enumerate(mean_profit)]
-            idx_profit.sort(key=itemgetter(1))
-            order[o] = [i[0] for i in idx_profit]
-
-        elif o == 'max_max_profit':
-            max_profit = np.max(data['value'], 0)
-            idx_profit = [(i, mp) for i, mp in enumerate(max_profit)]
-            idx_profit.sort(key=itemgetter(1), reverse=True)
-            order[o] = [i[0] for i in idx_profit]
-
-        elif o == 'min_max_profit':
-            max_profit = np.max(data['value'], 0)
-            idx_profit = [(i, mp) for i, mp in enumerate(max_profit)]
-            idx_profit.sort(key=itemgetter(1))
-            order[o] = [i[0] for i in idx_profit]
-
-        elif o == 'max_min_profit':
-            min_profit = np.min(data['value'], 0)
-            idx_profit = [(i, mp) for i, mp in enumerate(min_profit)]
-            idx_profit.sort(key=itemgetter(1), reverse=True)
-            order[o] = [i[0] for i in idx_profit]
-
-        elif o == 'min_min_profit':
-            min_profit = np.min(data['value'], 0)
-            idx_profit = [(i, mp) for i, mp in enumerate(min_profit)]
-            idx_profit.sort(key=itemgetter(1))
-            order[o] = [i[0] for i in idx_profit]
-
-        elif o == 'max_avg_profit_by_weight':
-            mean_profit = np.mean(data['value'], 0)
-            profit_by_weight = [v / w for v, w in zip(mean_profit, data['weight'])]
-            idx_profit_by_weight = [(i, f) for i, f in enumerate(profit_by_weight)]
-            idx_profit_by_weight.sort(key=itemgetter(1), reverse=True)
-            # print(idx_profit_by_weight)
-            order[o] = [i[0] for i in idx_profit_by_weight]
-
-        elif o == 'max_max_profit_by_weight':
-            max_profit = np.max(data['value'], 0)
-            profit_by_weight = [v / w for v, w in zip(max_profit, data['weight'])]
-            idx_profit_by_weight = [(i, f) for i, f in enumerate(profit_by_weight)]
-            idx_profit_by_weight.sort(key=itemgetter(1), reverse=True)
-            # print(idx_profit_by_weight)
-            order[o] = [i[0] for i in idx_profit_by_weight]
-
-    return order
 
 
 def get_smac_path(cfg, resource_path, pid):
@@ -398,7 +370,7 @@ def get_baseline_order(data, cfg, resource_path, pid):
         label_path = resource_path / 'labels' / cfg.problem.name / cfg.problem.size / f'label_{cfg.problem.size}.csv'
         df = pd.read_csv(label_path)
         incb = ast.literal_eval(df[df['pid'] == pid].iloc[0, 'incb'])
-        order, _ = get_variable_order_from_weights(data, incb)
+        order = get_variable_order(data=data, property_weights=incb, reverse=True)[0]
         orders.append(order)
 
     elif cfg.order_type == 'smac_all':
@@ -409,7 +381,7 @@ def get_baseline_order(data, cfg, resource_path, pid):
             traj = traj_path.open('r')
             lines = traj.readlines()
             property_weight = json.loads(lines[-1])
-            order, _ = get_variable_order_from_weights(data, property_weight['incumbent'])
+            order = get_variable_order(data=data, property_weights=property_weight['incumbent'], reverse=True)
             orders.append(order)
 
     return orders
