8854d7a0d048284c18c3bead5bb8fd4f95fa7ff7
rahulptel <rahul.polymtl@gmail.com>
2023-06-28 16:04:24 -0400

    [REF] Eval metrics in SmacD

--- code/python/learn2rank/trainer/trainer_smac_dataset.py
+++ code/python/learn2rank/trainer/trainer_smac_dataset.py
@@ -1,10 +1,19 @@
 import ast
+import logging
 from pathlib import Path
 
+import pandas as pd
+
 from learn2rank.utils.data import read_data_from_file
+from learn2rank.utils.metrics import eval_learning_metrics
+from learn2rank.utils.metrics import eval_order_metrics
+from learn2rank.utils.metrics import eval_rank_metrics
 from learn2rank.utils.order import get_variable_order
+from learn2rank.utils.order import get_variable_rank
+from learn2rank.utils.order import get_variable_score_from_weights
 from .trainer import Trainer
 
+log = logging.getLogger(__name__)
 
 class SmacDTrainer(Trainer):
     def __init__(self, data=None, model=None, cfg=None, rs=None, ps=None):
@@ -16,6 +25,10 @@ class SmacDTrainer(Trainer):
         self.traj_path = self.traj_path / f'{cfg.problem.acronym}_7_{cfg.problem.size}_0' / 'run_777' / 'traj.json'
         self.incb = ast.literal_eval(self.traj_path.read_text().strip().split('\n')[-1])["incumbent"]
 
+        self.dtrain = self._get_split_data(split='train')
+        self.dval = self._get_split_data(split='val')
+        self.dtest = self._get_split_data(split='test')
+
         if self.rs is None:
             self.rs = self._get_results_store()
             self.rs['task'] = self.cfg.task
@@ -24,34 +37,73 @@ class SmacDTrainer(Trainer):
         if self.ps is None:
             self.ps = self._get_preds_store()
 
-            names_tr, n_items_tr, wt_tr = self._get_split_data(split='train')
-            self.ps['train']['names'] = names_tr
-            self.ps['train']['n_items'] = n_items_tr
+            self.ps['train']['names'] = self.dtrain['names']
+            self.ps['train']['n_items'] = self.dtrain['n_items']
 
-            names_val, n_items_val, wt_val = self._get_split_data(split='val')
-            self.ps['val']['names'] = names_val
-            self.ps['val']['n_items'] = n_items_val
+            self.ps['val']['names'] = self.dval['names']
+            self.ps['val']['n_items'] = self.dval['n_items']
 
-            names_test, n_items_test, wt_test = self._get_split_data(split='test')
-            self.ps['test']['names'] = names_test
-            self.ps['test']['n_items'] = n_items_test
+            self.ps['test']['names'] = self.dtest['names']
+            self.ps['test']['n_items'] = self.dtest['n_items']
 
     def run(self):
-        self.ps['train']['order'] = self._get_split_order(split='train')
-        self.ps['val']['order'] = self._get_split_order(split='val')
-        self.val_tau = -1
+        self.ps['train']['score'] = self._get_split_scores(split='train')
+        self.ps['val']['score'] = self._get_split_scores(split='val')
+
+        self.ps['train']['order'] = get_variable_order(scores=self.ps['train']['score'], reverse=True)
+        self.ps['val']['order'] = get_variable_order(scores=self.ps['val']['score'], reverse=True)
+
+        self.ps['train']['rank'] = get_variable_rank(scores=self.ps['train']['score'], reverse=True)
+        self.ps['val']['rank'] = get_variable_rank(scores=self.ps['val']['score'], reverse=True)
+
+        # Eval learning metrics
+        log.info(f"* {self.cfg.model.name} Results")
+        log.info("** Train learning metrics:")
+        self.rs['train']['learning'] = eval_learning_metrics(self.dtrain['y'],
+                                                             self.ps['train']['rank'],
+                                                             sample_weight=self.dtrain['sample_weight'])
+        log.info("** Validation learning metrics:")
+        self.rs['val']['learning'] = eval_learning_metrics(self.dval['y'],
+                                                           self.ps['val']['rank'],
+                                                           sample_weight=self.dval['sample_weight'])
+
+        # Eval rank predictions
+        log.info("** Train order metrics:")
+        self.rs['train']['ranking'].extend(eval_order_metrics(get_variable_order(scores=self.dtrain['y']),
+                                                              self.ps['train']['order'],
+                                                              self.ps['train']['n_items']))
+        self.rs['train']['ranking'].extend(eval_rank_metrics(self.dtrain['y'],
+                                                             self.ps['train']['rank'],
+                                                             self.ps['train']['n_items']))
+        df_train = pd.DataFrame(self.rs['train']['ranking'], columns=['id', 'metric_type', 'metric_value'])
+        self.print_rank_metrics(df_train)
+
+        log.info("** Val order metrics:")
+        self.rs['val']['ranking'].extend(eval_order_metrics(get_variable_order(scores=self.dval['y']),
+                                                            self.ps['val']['order'],
+                                                            self.ps['val']['n_items']))
+        self.rs['val']['ranking'].extend(eval_rank_metrics(self.dval['y'],
+                                                           self.ps['val']['rank'],
+                                                           self.ps['val']['n_items']))
+        df_val = pd.DataFrame(self.rs['val']['ranking'], columns=['id', 'metric_type', 'metric_value'])
+        self.print_rank_metrics(df_val)
+        log.info(f"  {self.cfg.model.name} train time: {self.rs['time']['train']} \n")
+        self.val_tau = df_val.query("metric_type == 'kendall-coeff'")['metric_value'].mean()
 
         self._save_predictions()
         self._save_results()
 
     def predict(self, split='test'):
-        self.ps[split]['order'] = self._get_split_order(split=split)
+        self.ps[split]['score'] = self._get_split_scores(split=split)
+
+        self.ps[split]['order'] = get_variable_order(scores=self.ps[split]['score'], reverse=True)
+        self.ps[split]['rank'] = get_variable_rank(scores=self.ps[split]['score'], reverse=True)
 
         self._save_predictions()
         self._save_results()
 
-    def _get_split_order(self, split='train'):
-        y_pred_lst = []
+    def _get_split_scores(self, split='train'):
+        scores = []
 
         for name in self.ps[split]['names']:
             acronym, _, a, b, pid = name.split("_")
@@ -60,22 +112,6 @@ class SmacDTrainer(Trainer):
 
                 inst = self.inst_root_path / size / split / f'{name}.dat'
                 data = read_data_from_file(acronym, inst)
-                y_pred = get_variable_order(data=data, property_weights=self.incb, reverse=True)
-                y_pred_lst.append(y_pred)
+                scores.append(get_variable_score_from_weights(data, self.incb))
 
-        return y_pred_lst
-
-    def _get_split_data(self, split='train'):
-        x, y, wt, names, n_items = [], [], [], [], []
-
-        size = self.cfg.problem.size
-        # for size in self.cfg.dataset.size:
-        for v in self.data[size][split]:
-            _y = v['y']
-            n_items.append(self.cfg.problem.n_vars)
-            names.append(v['name'])
-            y.append(_y)
-
-        sample_weights = [1] * len(y)
-
-        return names, n_items, sample_weights
+        return scores
